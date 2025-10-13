0ad34606330a93df9ed43a1d23d02be56bc5dc31
Иван <@DESETPICON>
2025-10-14 01:04:44 +0500

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Выберите номер метода или введите число 21 для выхода из программы: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x == 21)
+                {
+                    break;
+                }
+                Methods methods = new Methods();
+                switch (x)
+                {
+                    case 1: 
+                        Console.Write("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int num = methods.sumLastNums(x);
+                        Console.WriteLine("Сумма чисел равна: " + num); break;
+                    case 2: 
+                        Console.Write("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(methods.isPositive(x)); break;
+                    case 3:
+                        Console.Write("Введите символ: ");
+                        string str = Console.ReadLine();
+                        char letter = str[0];
+                        Console.WriteLine(methods.isUpperCase(letter));
+                        break;
+                    case 4:                  
+                        Console.Write("Введите первое число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int a = x;
+                        Console.Write("Введите второе число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int b = x;
+                        Console.WriteLine(methods.isDivisor(a,b));
+                        break;
+                    case 5:
+                        Console.Write("Введите первое число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        a = x;
+                        Console.Write("Введите второе число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        b = x;
+                        Console.WriteLine(methods.lastNumSum(a, b));
+                        break;
+                    case 6:
+                        Console.Write("Введите число X: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        a = x;
+                        Console.Write("Введите число Y: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        b = x;
+                        Console.WriteLine(methods.safeDiv(a, b));
+                        break;
+                    case 7:
+                        Console.Write("Введите первое число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        a = x;
+                        Console.Write("Введите второе число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        b = x;
+                        Console.WriteLine(methods.makeDecision(a, b));
+                        break;
+                    case 8:
+                        Console.Write("Введите первое число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        a = x;
+                        Console.Write("Введите второе число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        b = x;                
+                        Console.Write("Введите третье число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int c = x;
+                        Console.WriteLine(methods.sum3(a, b, c));
+                        break;
+                    case 9:
+                        Console.Write("Введите ваш возраст: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(methods.age(x));
+                        break;
+                    case 10:
+                        Console.Write("Введите день недели: ");
+                        str = Console.ReadLine();
+                        methods.printDays(str);
+                        break;
+                    case 11:
+                        Console.Write("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(methods.reverseListNums(x));
+                        break;
+                    case 12:
+                        Console.Write("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        a = x;
+                        Console.Write("Введите степень числа: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        b = x;
+                        Console.WriteLine(methods.pow(a, b));
+                        break;
+                    case 13:
+                        Console.Write("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(methods.equalNum(x));
+                        break;
+                    case 14:
+                        Console.Write("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        methods.leftTriangle(x);
+                        break;
+                    case 15:
+                        methods.guessGame();
+                        break;
+                    case 16:
+                        Console.Write("Введите размер массива: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int[] arr = new int[x];
+                        Console.WriteLine($"Введите {x} чисел:");
+
+                        for (int i = 0; i < x; i++)
+                        {
+                            Console.Write($"Элемент {i + 1}: ");
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        Console.Write("Введите число для поиска: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(methods.findLast(arr, x));
+                        break;
+                    case 17:
+                        Console.Write("Введите размер массива: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        arr = new int[x];
+                        Console.WriteLine($"Введите {x} чисел:");
+
+                        for (int i = 0; i < x; i++)
+                        {
+                            Console.Write($"Элемент {i + 1}: ");
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        Console.Write("Введите новый элемент для добавления в массив: ");
+                        x = Convert.ToInt32(Console.ReadLine());                     
+                        Console.Write("Введите номер позиции в которую будет добавлен новый элемент: ");
+                        int pos = Convert.ToInt32(Console.ReadLine());
+                        int[] resultArr = methods.add(arr, x, pos);
+                        Console.WriteLine("Новый массив: [" + string.Join(", ", resultArr) + "]");
+                        break;
+                    case 18:
+                        Console.Write("Введите размер массива: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        arr = new int[x];
+                        Console.WriteLine($"Введите {x} чисел:");
+
+                        for (int i = 0; i < x; i++)
+                        {
+                            Console.Write($"Элемент {i + 1}: ");
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        methods.reverse(arr);
+                        Console.WriteLine("Новый массив: [" + string.Join(", ", arr) + "]");
+                        break;
+                    case 19:
+                        Console.Write("Введите размер массива 1: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int[] arr1 = new int[x];
+                        Console.WriteLine($"Введите {x} чисел для первого массива:");
+
+                        for (int i = 0; i < x; i++)
+                        {
+                            Console.Write($"Элемент {i + 1}: ");
+                            arr1[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.Write("Введите размер массива 2: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        int[] arr2 = new int[x];
+                        Console.WriteLine($"Введите {x} чисел для второго массива:");
+
+                        for (int i = 0; i < x; i++)
+                        {
+                            Console.Write($"Элемент {i + 1}: ");
+                            arr2[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        resultArr = methods.concat(arr1, arr2);
+                        Console.WriteLine("Объединенный массив: " + string.Join(", ", resultArr));
+                        break;
+                    case 20:
+                        Console.Write("Введите размер массива: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        arr = new int[x];
+                        Console.WriteLine($"Введите {x} чисел:");
+
+                        for (int i = 0; i < x; i++)
+                        {
+                            Console.Write($"Элемент {i + 1}: ");
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        resultArr = methods.deleteNegative(arr);
+                        Console.WriteLine("Массив без отрицательных значений: " + string.Join(", ", resultArr));
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ lab1/methods.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Methods
+    {
+        public int sumLastNums(int x) /// 1
+        {
+            return (x % 10) + (x % 100 / 10);
+        }
+        public bool isPositive(int x) /// 2
+        {
+            return x >= 0;
+        }
+        public bool isUpperCase(char x) /// 3
+        {
+            return x >= 'A' && x <= 'Z';
+        }
+        public bool isDivisor(int a, int b) /// 4
+        {
+            return  (a % b) == 0 || (b % a) == 0;
+        }
+        public int lastNumSum(int a, int b) /// 5
+        {
+            return (a % 10) + (b % 10);
+        }
+        public double safeDiv(int x, int y) /// 6
+        {
+            if (y == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+        public String makeDecision(int x, int y) /// 7
+        {          
+            if (x > y)
+            {              
+                return x + " > " + y;
+            }
+            else if (x < y)
+            {     
+                return x + " < " + y;
+            }
+            else
+            {              
+                return x + " == " + y;
+            }
+        }
+        public bool sum3(int x, int y, int z) /// 8
+        {
+            if (x + y == z) 
+            {
+                return x + y == z;
+            }
+            else if (x + z == y)
+            {
+                return x + z == y;
+            }
+            else if (y + z == x)
+            {
+                return y + z == x;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public String age(int x) /// 9
+        {
+            if (x % 10 == 1 && x != 11)
+            {
+                return "Вам " + x + " год";
+            }
+            else if ((x % 10 >= 2 && x % 10 <= 4) && ! (x >= 12 && x <= 14))
+            {
+                return "Вам " + x + " года";
+            }
+            else
+            {
+                return "Вам " + x + " лет";
+            }
+        }
+        public void printDays(string x) /// 10
+        {
+            switch (x.ToLower())
+            {
+                case "понедельник":
+                    Console.WriteLine("понедельник");
+                    goto case "вторник";
+                case "вторник":
+                    Console.WriteLine("вторник");
+                    goto case "среда";
+                case "среда":
+                    Console.WriteLine("среда");
+                    goto case "четверг";
+                case "четверг":
+                    Console.WriteLine("четверг");
+                    goto case "пятница";
+                case "пятница":
+                    Console.WriteLine("пятница");
+                    goto case "суббота";
+                case "суббота":
+                    Console.WriteLine("суббота");
+                    goto case "воскресенье";
+                case "воскресенье":
+                    Console.WriteLine("воскресенье");
+                    break;
+                default:
+                    Console.WriteLine("это не день недели");
+                    break;
+            }
+        }
+        public String reverseListNums(int x) /// 11
+        {
+            string result = "";
+
+            while (x >= 0)
+            {
+                result += x + " ";
+                x--;
+            }
+
+            return result;
+        }
+        public int pow(int x, int y) /// 12
+        {
+            int pow = 1;
+
+            for (int i = 0; i < y; i++)
+            {
+                pow *= x; 
+            }
+
+            return pow;
+        }
+        public bool equalNum(int x) /// 13
+        {
+            if (x == 0)
+            {
+                return true;
+            } 
+
+            if (x < 0)
+            {
+                x = -x;
+            }
+
+            int lastDigit = x % 10;
+
+            while (x > 0)
+            {
+                if (x % 10 != lastDigit) 
+                {
+                    return false;
+                }     
+                
+                x /= 10;
+            }
+
+            return true;
+        }
+        public void leftTriangle(int x) /// 14
+        {
+            for (int i = 1; i <= x; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+        }
+        public void guessGame() /// 15
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(0, 10); // Генерируем число от 0 до 9
+            int attempts = 0;
+            int userGuess;
+
+            Console.WriteLine("Угадайте число от 0 до 9!");
+
+            do
+            {
+                Console.Write("Введите число от 0 до 9: ");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                attempts++;
+
+                if (userGuess == secretNumber)
+                {
+                    Console.WriteLine("Вы угадали!");
+                }
+                else
+                {
+                    Console.WriteLine("Вы не угадали, введите число от 0 до 9:");
+                }
+            }
+            while (userGuess != secretNumber);
+
+            Console.WriteLine($"Вы отгадали число за {attempts} попыт(ки/ок)");
+        }
+        public int findLast(int[] arr, int x) /// 16
+        {
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (arr[i] == x)
+                {
+                    return i + 1;
+                }   
+            }
+
+            return -1;
+        }
+        public int[] add(int[] arr, int x, int pos) /// 17
+        {
+            
+            if (pos < 0 || pos > arr.Length)
+            {
+                Console.WriteLine($"Ошибка: позиция {pos} выходит за границы массива");
+                return arr;
+            }
+
+            int[] newArr = new int[arr.Length + 1];
+
+            for (int i = 0; i < pos; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            newArr[pos] = x;
+
+            for (int i = pos; i < arr.Length; i++)
+            {
+                newArr[i + 1] = arr[i];
+            }
+            return newArr;
+        }
+        public void reverse(int[] arr) /// 18
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left < right)
+            {               
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        public int[] concat(int[] arr1, int[] arr2) // 19
+        {
+            int[] newArr = new int[arr1.Length + arr2.Length];           
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                newArr[i] = arr1[i];            
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                newArr[i+arr1.Length] = arr2[i];
+            }
+            return newArr;
+        }
+        public int[] deleteNegative(int[] arr) // 20
+        {
+            
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0)
+                    count++;
+            }
+
+            int[] result = new int[count];
+
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0)
+                {
+                    result[index] = arr[i];
+                    index++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
